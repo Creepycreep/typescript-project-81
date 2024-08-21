dccb72d76b9f9076603ce58bfb1079b499c09d46
Elizaveta Shevtsova <111qw95@gmail.com>
2024-08-21 22:58:50 +0300

    feat: add GH rules

--- src/modules/Tag/Tag.ts
+++ src/modules/Tag/Tag.ts
@@ -1,36 +1,37 @@
 export default class Tag {
-  private unpairedTags: string[] = [
-    "area",
-    "base",
-    "br",
-    "col",
-    "embed",
-    "hr",
-    "img",
-    "input",
-    "link",
-    "meta",
-    "source",
-    "track",
-    "wbr"
-  ];
+	private unpairedTags: string[] = [
+		'area',
+		'base',
+		'br',
+		'col',
+		'embed',
+		'hr',
+		'img',
+		'input',
+		'link',
+		'meta',
+		'source',
+		'track',
+		'wbr',
+	];
 
-  private isUnpaired: boolean
-  private name: string
-  private attributes?: string
-  private content?: string
+	private isUnpaired: boolean;
+	private name: string;
+	private attributes?: string;
+	private content?: string;
 
-  constructor(name: string, attributes?: Record<string, string>, content?: string) {
-    this.name = name;
-    this.content = content ?? '';
-    this.isUnpaired = this.unpairedTags.includes(name);
-    this.attributes = Object.entries(attributes ?? {})
-      .map(([key, value]) => `${key}="${value}"`)
-      .join(' ')
+	constructor(name: string, attributes?: Record<string, string>, content?: string) {
+		this.name = name;
+		this.content = content ?? '';
+		this.isUnpaired = this.unpairedTags.includes(name);
+		this.attributes = Object.entries(attributes ?? {})
+			.map(([key, value]) => `${key}="${value}"`)
+			.join(' ');
+	}
 
-  }
-
-  toString() {
-    return this.isUnpaired ? `<${this.name} ${this.attributes}>` : `<${this.name} ${this.attributes}>${this.content}</${this.name}>`
-  }
+	toString() {
+		return this.isUnpaired
+			? `<${this.name} ${this.attributes}>`
+			: `<${this.name} ${this.attributes}>${this.content}</${this.name}>`;
+	}
 }
